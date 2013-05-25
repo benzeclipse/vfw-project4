400a380c4c2b860c60c8630ebb7d11842eed9baa
Ben <ben@Bens-MacBook-Pro.local>
2013-05-25 13:47:37 -0700

    final update

--- js/json.js
+++ js/json.js
@@ -1,38 +1,43 @@
+// VFW
+// Term 1305
+// Banchop Ben Kangdouangnhot
+// Project 4
+
 var json = {
-	"Info1" : {
-			"sport"			: ["Sports: ",	"NFL"],
-			"tname"			: ["Team Name: ", "Carolina Panthers"],
-			"name"			: ["Name: ", "Ben Kang"],
-			"group" 		: ["More Stuff: ", "Tickets"],  
-			"aDate"			: ["Date: ", "1999-09-09"],
-			"range"			: ["Tickets Desired: ",	"100"],
-			"season"		: ["Season: ", "single"],         
-			"payments"		: ["Payments: ", "Credit Card"],	
-			"concerns" 		: ["Concerns: ", "want cheap tickets"]
+	"Info1" : {	
+			"sport"			: ["Sports ",		 	 " NFL"],
+			"tname"			: ["Team Name ",		 "Carolina Panthers"],
+			"name"			: ["Name ", 			 "Ben Kang"],
+			"group" 		: ["More Stuff ", 		 "Tickets"],  
+			"aDate"			: ["Date ", 		 	 "1999-09-09"],
+			"range"			: ["Tickets Desired ",	 "100"],
+			"season"		: ["Season ", 			 "single"],         
+			"payments"		: ["Payments ",			 "Credit Card"],	
+			"concerns" 		: ["Concerns ", 		 "want cheap tickets"]
 	},
 
 	"Info2" : {
-			"sport"			: ["Sports: ",	"NBA"],
-			"tname"			: ["Team Name: ", "Charlotte Bobcats"],
-			"name"			: ["Name: ", "Michael Jordan"],
-			"group" 		: ["More Stuff: ", "Souvenirs"], 
-			"aDate"			: ["Date: ", "2013-05-23"],
-			"range"			: ["Tickets Desired: ",	"100"],
-			"season"		: ["Season: ", "season"],         
-			"payments"		: ["Payments: ", "cash"],	
-			"concerns" 		: ["Concerns: ", "Best seats in the house"]
+			"sport"			: ["Sports ",			 "NBA"],
+			"tname"			: ["Team Name ",		 "Charlotte Bobcats"],
+			"name"			: ["Name ", 			 "Michael Jordan"],
+			"group" 		: ["More Stuff ",		 "Souvenirs"], 
+			"aDate"			: ["Date ", 			 "2013-05-23"],
+			"range"			: ["Tickets Desired ", 	 "100"],
+			"season"		: ["Season ", 			 "season"],         
+			"payments"		: ["Payments ",          "cash"],	
+			"concerns" 		: ["Concerns ", 		 "Best seats in the house"]
 	},
 
 	"Info3" : {
-			"sport"			: ["Sports: ",	"BaseBall"],
-			"tname"			: ["Team Name: ", "LA Dodgers"],
-			"name"			: ["Name: ", "Joe Smoe"],
-			"group" 		: ["More Stuff: ", "Apparels"], 
-			"aDate"			: ["Date: ", "2013-05-23"],
-			"range"			: ["Tickets Desired: ",	"50"],
-			"season"		: ["Season: ", "single"],         
-			"payments"		: ["Payments: ", "check"],	
-			"concerns" 		: ["Concerns: ", "any seats are okay!"]
+			"sport"			: ["Sports ",			 "BaseBall"],
+			"tname"			: ["Team Name ",		 "LA Dodgers"],
+			"name"			: ["Name ", 			 "Joe Smoe"],
+			"group" 		: ["More Stuff ",		 "Apparels"], 
+			"aDate"			: ["Date ", 			 "2013-05-23"],
+			"range"			: ["Tickets Desired ",	 "50"],
+			"season"		: ["Season ",			 "single"],         
+			"payments"		: ["Payments ", 		 "check"],	
+			"concerns" 		: ["Concerns ", 		 "any seats are okay!"]
 	}
 
 }
